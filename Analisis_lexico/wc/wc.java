62973c773ddba60dddd98ec5646978df9fa2ce24
Javier Márquez <javier0marquezr@gmail.com>
2022-06-11 21:19:08 +0200

    fix(wc.java): faltaban imports

--- Analisis_lexico/wc/wc.java
+++ Analisis_lexico/wc/wc.java
@@ -1,8 +1,11 @@
+import java.io.IOException;
+import java.io.FileReader;
+
 public class wc {
 
 	public static void main(String[] args){
 	
-		if(args.length = 0) return;
+		if(args.length() = 0) return;
 		
 		try{
 			Yylex lex = new Yylex(new FileReader(args[0]));
@@ -11,9 +14,9 @@ public class wc {
 			
 			}
 
-			System.out.print(lex.lineas.toString());
-			System.out.print(lex.palabras.toString());
-			System.out.print(lex.caracteres.toString());
+			System.out.print(lex.lineas);
+			System.out.print(lex.palabras);
+			System.out.print(lex.caracteres);
 			System.out.print(args[0]);
 			
 		} catch(IOException e){}
--- Analisis_lexico/wc/wc.lex
+++ Analisis_lexico/wc/wc.lex
@@ -4,11 +4,12 @@
 %line /*switches line counting on (yyline)*/
 
 %{
-	public int lineas = 0;
-	public int palabras = 0;
-	public int caracteres = 0;
+	int lineas = 0;
+	int palabras = 0;
+	int caracteres = 0;
 %}
 
 %%
 
-[^] {/*Ignorar*/}
+<<EOF>> 			{lineas = yyline;} //El num. lineas es la linea en la que esta el EOF.
+[^] 				{/*Ignorar*/}
