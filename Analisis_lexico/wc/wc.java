a1f2a5b17305ad324335dab45e27bf61e0564a22
Javier Márquez <javier0marquezr@gmail.com>
2022-06-11 22:03:38 +0200

    test(wc.lex): no pasa prueba2
    Hay palabras que no reconoce. El conteo de caracteres y lineas funciona

--- Analisis_lexico/wc/wc.java
+++ Analisis_lexico/wc/wc.java
@@ -4,7 +4,7 @@ import java.io.FileReader;
 public class wc {
 	public static void main(String[] args){
 	
-		if(args.length <= 0) return;
+		if(args.length == 0) return;
 		
 		try{
 			Yylex lex = new Yylex(new FileReader(args[0]));
@@ -12,10 +12,10 @@ public class wc {
 			while(lex.yylex() != -1) {
 	                
 	        }
-			System.out.print(lex.lineas);
-			System.out.print(lex.palabras);
-			System.out.print(lex.caracteres);
-			System.out.print(args[0]);
+			System.out.print(lex.lineas + "\t");
+			System.out.print(lex.palabras + "\t");
+			System.out.print(lex.caracteres + " ");
+			System.out.println(args[0]);
 			
 		} catch(IOException e){}
 	}
