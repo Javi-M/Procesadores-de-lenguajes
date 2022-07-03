18b919c84f2a27b4fd6db94bfcea06d04596836c
Javier Márquez <javier0marquezr@gmail.com>
2022-07-03 09:57:49 +0200

    Mod: estructura LL1-sent-WHILE

---
+++ Analisis_sintactico/LL1-sent-WHILE/Parser.java
@@ -0,0 +1,32 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileReader;
+
+class Parser {
+
+private static int token;
+private static Yylex lex;
+private static int yylex() {
+	int token = 0;
+	try {
+		token = lex.yylex();
+	} catch (IOException e) {
+		System.out.println("IOException");
+		System.exit(0);
+	}
+	return token;
+}
+
+public static void main(String[] arg) {
+    if (arg.length>0) {
+        try {
+            lex = new Yylex(new FileReader(arg[0]));
+            /// .... A completar .....
+        } catch (IOException e) {
+        } 
+    }
+}
+
+/// .... A completar .....
+
+}
