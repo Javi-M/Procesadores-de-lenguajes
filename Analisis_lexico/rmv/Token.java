21f2aac6b3e5cdccf119c64daf3ba0334c262360
Javi-M <89javier98@uma.es>
2025-09-29 18:23:51 +0200

    Add changes from local

--- Analisis_lexico/rmv/Token.java
+++
@@ -1,9 +0,0 @@
-public class Token 
-{
-	public static final int SIMPLE_VAR_ID  = 1;
-	public static final int COMPLEX_VAR_ID = 2;
-	public static final int SIMPLE_VALUE	 = 3;
-	public static final int COMPLEX_VALUE  = 4;
-	public static final int OTHER					 = 5; // Por ver
-	public static final int EOI 					 = 6; // End Of Instruction
-}
